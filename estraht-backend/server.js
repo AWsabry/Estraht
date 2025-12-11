7a72be034ead6b391b3bf0962fb1dbdc7f22d598
AWsabry <awsabry2000@gmail.com>
2025-12-11 21:02:51 +0200

    backend_is_deployed

--- estraht-backend/server.js
+++ estraht-backend/server.js
@@ -3,7 +3,7 @@ import app from "./src/app.js";
 
 dotenv.config();
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5051;
 
 const server = app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
